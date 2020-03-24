d75989b13a3947afdbe2c179bd3a25ab39bccea3
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-03-24 15:40:25 +0530

    added parcel and dev dependencies

---
+++ src/Pet.js
@@ -0,0 +1,9 @@
+import React from "react";
+// your code is going to go here, for now
+export default function Pet({ name, animal, breed }) {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h3", {}, breed),
+  ]);
+}
